bea1919433e4c67f0bcf6f608e382f188c4188a3
Nir Channes <nirh2@yahoo.com>
2017-11-23 12:21:08 +0200

    car details view and controller working

--- app/details/carDetailsCtrl.js
+++ app/details/carDetailsCtrl.js
@@ -1,4 +1,6 @@
 
 carApp.controller("carDetailsCtrl", function($scope, cars, $log, $routeParams){
-    $scope.test = $routeParams.index;
+    //$scope.test = $routeParams.index;
+
+    $scope.car = cars.getCarById($routeParams.index);
 })
