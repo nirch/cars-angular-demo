5e1a612557b26b79a06d46baff6d406aab9eefb1
Nir Channes <nirh2@yahoo.com>
2017-11-23 11:54:34 +0200

    http call in gallery works

--- app/gallery/carGalleryCtrl.js
+++ app/gallery/carGalleryCtrl.js
@@ -1,4 +1,18 @@
 
 carApp.controller("carGalleryCtrl", function($scope, cars, $log, $http) {
-    $scope.test = "ewewew";
+    //$scope.test = "ewewew";
+
+    // http call to get data from json
+    $http.get("app/model/data/cars.json").then(function mySuccess(response) {
+        // Updating the service with the data
+        cars.setCars(response.data);
+
+        // Getting the data from the service
+        $scope.cars = cars.getAll();
+      }, function myError(response) {
+        alert("error" + JSON.stringify(response.status));
+      });
+
+    
+    
 })
