e354b8419d0ed06fc08b55e4ecb7a638c5c55e90
Nir Channes <nirh2@yahoo.com>
2017-11-23 11:59:49 +0200

    copy paste of gallery code working

--- app/gallery/carGalleryCtrl.js
+++ app/gallery/carGalleryCtrl.js
@@ -11,8 +11,42 @@ carApp.controller("carGalleryCtrl", function($scope, cars, $log, $http) {
         $scope.cars = cars.getAll();
       }, function myError(response) {
         alert("error" + JSON.stringify(response.status));
-      });
+    });
 
+    $scope.addCar = function() {
+        cars.addCar(new Car("Susita", "Carmel", "1972", 12022939));
+        $scope.cars = cars.getAll();
+    }
+      
+      $scope.sortBy = function(prop) {
+        $scope.orderProp = prop;
+      }
+      
+      
+      // Custom filter function
+      $scope.filterBrandAndModel = function(car) {
+        if ($scope.query == undefined || $scope.query === "") {
+          return true;
+        } 
+        
+        var queryLowerCase = $scope.query.toLowerCase();
+        var brand = car.brand.toLowerCase();
+        var model = car.model.toLowerCase();
+        
+        if (brand.includes(queryLowerCase) || 
+            model.includes(queryLowerCase)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      
+      $scope.selectCar = function (car) {
+        car.selected = !car.selected;
+      }
+        
+
+    
     
     
 })
