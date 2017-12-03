aa75a285cb19867a153eac84283de78108af2b91
Nir Channes <nirh2@yahoo.com>
2017-12-03 09:46:40 +0200

    google charts completed

--- app/charts/chartsCtrl.js
+++ app/charts/chartsCtrl.js
@@ -1,3 +1,35 @@
 carApp.controller("chartsCtrl", function($scope, cars, $log){
-    $scope.test = "blabla"
+    $scope.myChartObject = {};
+    
+    $scope.myChartObject.type = "PieChart";
+
+    var newCars = 0;
+    var oldCars = 0;
+    for (var i = 0; i < cars.getAll().length; i++) {
+        var currentCar = cars.getCarById(i);
+        if (currentCar.year >= 2010) {
+            newCars++;
+        } else {
+            oldCars++;
+        }
+    }
+
+
+    $scope.myChartObject.data = {"cols": [
+        {id: "t", label: "New or Old", type: "string"},
+        {id: "s", label: "Number of Cars", type: "number"}
+    ], "rows": [
+        {c: [
+            {v: "New Cars (>2010)"},
+            {v: newCars},
+        ]},
+        {c:  [
+            {v: "Old Cars"},
+            {v: oldCars},
+        ]}
+    ]};
+
+    $scope.myChartObject.options = {
+        'title': 'New vs. Old Cars'
+    };
 });
