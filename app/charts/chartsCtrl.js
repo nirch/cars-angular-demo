e5b57e52e210b7caefd3a2da9c3b90636756cc01
Nir Channes <nirh2@yahoo.com>
2017-12-06 09:05:05 +0200

    fix for chart

--- app/charts/chartsCtrl.js
+++ app/charts/chartsCtrl.js
@@ -1,33 +1,46 @@
-carApp.controller("chartsCtrl", function($scope, cars, $log){
+carApp.controller("chartsCtrl", function ($scope, cars, $log) {
     $scope.myChartObject = {};
-    
+
     $scope.myChartObject.type = "PieChart";
 
-    var newCars = 0;
-    var oldCars = 0;
-    for (var i = 0; i < cars.getAll().length; i++) {
-        var currentCar = cars.getCarById(i);
-        if (currentCar.year >= 2010) {
-            newCars++;
-        } else {
-            oldCars++;
+    cars.getAll().then(function mySuccess(data) {
+        var newCars = 0;
+        var oldCars = 0;
+        for (var i = 0; i < data.length; i++) {
+            var currentCar = cars.getCarById(i);
+            if (currentCar.year >= 2010) {
+                newCars++;
+            } else {
+                oldCars++;
+            }
         }
-    }
+
+        $scope.myChartObject.data = {
+            "cols": [
+                { id: "t", label: "New or Old", type: "string" },
+                { id: "s", label: "Number of Cars", type: "number" }
+            ], "rows": [
+                {
+                    c: [
+                        { v: "New Cars (>2010)" },
+                        { v: newCars },
+                    ]
+                },
+                {
+                    c: [
+                        { v: "Old Cars" },
+                        { v: oldCars },
+                    ]
+                }
+            ]
+        };        
+    }, function myError(responseError) {
+        alert(JSON.stringify(responseError));
+    });
 
 
-    $scope.myChartObject.data = {"cols": [
-        {id: "t", label: "New or Old", type: "string"},
-        {id: "s", label: "Number of Cars", type: "number"}
-    ], "rows": [
-        {c: [
-            {v: "New Cars (>2010)"},
-            {v: newCars},
-        ]},
-        {c:  [
-            {v: "Old Cars"},
-            {v: oldCars},
-        ]}
-    ]};
+
+
 
     $scope.myChartObject.options = {
         'title': 'New vs. Old Cars'
