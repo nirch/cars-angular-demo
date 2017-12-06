ec6543179521dcb23b9bd602f4e5e3d4e2ec77c6
Nir Channes <nirh2@yahoo.com>
2017-12-06 08:55:45 +0200

    moving http into service (using $q)

--- app/gallery/carGalleryCtrl.js
+++ app/gallery/carGalleryCtrl.js
@@ -1,56 +1,51 @@
 
-carApp.controller("carGalleryCtrl", function($scope, cars, $log, $http, $location) {
-    //$scope.test = "ewewew";
+carApp.controller("carGalleryCtrl", function ($scope, cars, $log, $http, $location) {
+  //$scope.test = "ewewew";
 
-    // http call to get data from json
-    $http.get("app/model/data/cars.json").then(function mySuccess(response) {
-        // Updating the service with the data
-        cars.setCars(response.data);
+  cars.getAll().then(function mySuccess(data) {
+    $scope.cars = data;
+  }, function myError(responseError) {
+    alert(JSON.stringify(responseError));
+  });
 
-        // Getting the data from the service
-        $scope.cars = cars.getAll();
-      }, function myError(response) {
-        alert("error" + JSON.stringify(response.status));
-    });
+  $scope.addCar = function () {
+    cars.addCar(new Car("Susita", "Carmel", "1972", 12022939));
+    $scope.cars = cars.getAll();
+  }
 
-    $scope.addCar = function() {
-        cars.addCar(new Car("Susita", "Carmel", "1972", 12022939));
-        $scope.cars = cars.getAll();
+  $scope.sortBy = function (prop) {
+    $scope.orderProp = prop;
+  }
+
+
+  // Custom filter function
+  $scope.filterBrandAndModel = function (car) {
+    if ($scope.query == undefined || $scope.query === "") {
+      return true;
     }
-      
-      $scope.sortBy = function(prop) {
-        $scope.orderProp = prop;
-      }
-      
-      
-      // Custom filter function
-      $scope.filterBrandAndModel = function(car) {
-        if ($scope.query == undefined || $scope.query === "") {
-          return true;
-        } 
-        
-        var queryLowerCase = $scope.query.toLowerCase();
-        var brand = car.brand.toLowerCase();
-        var model = car.model.toLowerCase();
-        
-        if (brand.includes(queryLowerCase) || 
-            model.includes(queryLowerCase)) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-      
-      $scope.openCar = function (car) {
-        // Getting the index of the car in the array
-        var carIndex = $scope.cars.indexOf(car);
 
-        // Updating the URL
-        $location.path("/cars/" + carIndex)
-      }
-        
+    var queryLowerCase = $scope.query.toLowerCase();
+    var brand = car.brand.toLowerCase();
+    var model = car.model.toLowerCase();
 
-    
-    
-    
+    if (brand.includes(queryLowerCase) ||
+      model.includes(queryLowerCase)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  $scope.openCar = function (car) {
+    // Getting the index of the car in the array
+    var carIndex = $scope.cars.indexOf(car);
+
+    // Updating the URL
+    $location.path("/cars/" + carIndex)
+  }
+
+
+
+
+
 })
--- app/model/cars.js
+++ app/model/cars.js
@@ -1,9 +1,31 @@
 
-carApp.factory("cars", function(Car) {
+carApp.factory("cars", function (Car, $http, $q) {
     var cars = [];
+    var wasLoaded = false;
 
     function getAll() {
-        return cars;
+        // Creating a promise object
+        var deferred = $q.defer();
+
+        if (wasLoaded) {
+            deferred.resolve(cars);
+        } else {
+            wasLoaded = true;
+            // http call to get data from json
+            $http.get("app/model/data/cars.json").then(function mySuccess(response) {
+                // Updating the service with the data
+                setCars(response.data);
+
+                // Returning a successful async response with the cars
+                deferred.resolve(cars);
+            }, function myError(response) {
+                // Retrurning an error async response
+                deferred.reject(response);
+            });
+        }
+
+        // Returning a promise to get an async response later
+        return deferred.promise;
     }
 
     function getCarById(index) {
@@ -17,10 +39,12 @@ carApp.factory("cars", function(Car) {
     function setCars(carsPlain) {
         cars = [];
         for (var i = 0; i < carsPlain.length; i++) {
-            cars.push(new Car(carsPlain[i].brand, carsPlain[i].model, 
+            cars.push(new Car(carsPlain[i].brand, carsPlain[i].model,
                 carsPlain[i].year, carsPlain[i].km))
         }
     }
+
+
 
     return {
         getAll: getAll,
